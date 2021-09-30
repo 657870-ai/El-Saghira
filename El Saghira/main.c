dc6df302da173aaf1b85a944eb60746411fae540
Andres Barbudo Rodriguez <nerdcorestyle@gmail.com>
2021-09-29 21:53:09 -0300

    Initial Commit

---
+++ El Saghira/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  El Saghira
+//
+//  Created by Andres Barbudo Rodriguez on 29-09-21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
